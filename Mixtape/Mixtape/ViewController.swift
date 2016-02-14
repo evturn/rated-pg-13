74e2ec1ed8887244e9901c6e2e4409b8149c9ee7
Evan Turner <evturn@gmail.com>
2016-02-14 00:15:27 -0500

    Add new project, configure initial UI

---
+++ Mixtape/Mixtape/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Mixtape
+//
+//  Created by Evan Turner on 2/14/16.
+//  Copyright © 2016 Evan Turner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var aButton: UIButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    aButton.setTitle("Press down firmly.", forState: .Normal)
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  @IBAction func buttonPressed() {
+    view.backgroundColor = UIColor.orangeColor()
+  }
+
+}
+
