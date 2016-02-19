c71e43570ccd7a3a372365a34eb432da823b1761
Evan Turner <evturn@gmail.com>
2016-02-19 12:19:11 -0500

    Create for..in loop for all playlosts

--- Mixtape/Mixtape/PlaylistMasterViewController.swift
+++ Mixtape/Mixtape/PlaylistMasterViewController.swift
@@ -10,13 +10,22 @@ import UIKit
 
 class PlaylistMasterViewController: UIViewController {
 
+  var playlistsArray: [UIImageView] = []
   @IBOutlet weak var playlistImageView0: UIImageView!
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    playlistsArray.append(playlistImageView0)
     
-    let playlist = Playlist(index: 0)
-    playlistImageView0.image = playlist.icon
+    for index in 0..<playlistsArray.count {
+      let playlist = Playlist(index: index)
+      let playlistImageView = playlistsArray[index]
+      
+      playlistImageView.image = playlist.icon
+      playlistImageView.backgroundColor = playlist.backgroundColor
+    }
+    
+    playlistsArray.append(playlistImageView0)
   }
 
   override func didReceiveMemoryWarning() {
@@ -30,9 +39,14 @@ class PlaylistMasterViewController: UIViewController {
   
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     if segue.identifier == "showPlaylistDetailSegue" {
-      let playlistDetailController = segue.destinationViewController as! PlaylistDetailViewController
       
-      playlistDetailController.playlist = Playlist(index: 0)
+      let playlistImageView = sender!.view as! UIImageView
+      if let index = playlistsArray.indexOf(playlistImageView) {
+        let playlistDetailController = segue.destinationViewController as! PlaylistDetailViewController
+        playlistDetailController.playlist = Playlist(index: index)
+      }
+      
+
     }
   }
 
