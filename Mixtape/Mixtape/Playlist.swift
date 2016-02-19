68e6572327fedb889f36c237d58a035e6d2a19d0
Evan Turner <evturn@gmail.com>
2016-02-19 14:36:58 -0500

    Fix made to Playlist struct

--- Mixtape/Mixtape/Playlist.swift
+++ Mixtape/Mixtape/Playlist.swift
@@ -19,30 +19,29 @@ struct Playlist {
   
   init(index: Int) {
     let musicLibrary = MusicLibrary().library
-    let playListDictionary = musicLibrary[index]
+    let playlistDictionary = musicLibrary[index]
     
-    title = playListDictionary["title"] as? String!
-    description = playListDictionary["description"] as? String!
+    title = playlistDictionary["title"] as? String
+    description = playlistDictionary["description"] as? String
     
-    let iconName = playListDictionary["icon"] as? String!
-    icon = UIImage(named: iconName!)
+    let iconName = playlistDictionary["icon"] as! String
+    let largeIconName = playlistDictionary["largeIcon"] as! String
+    icon = UIImage(named: iconName)
+    largeIcon = UIImage(named: largeIconName)
     
-    let largeIconName = playListDictionary["largeIcon"] as? String!
-    largeIcon = UIImage(named: largeIconName!)
-   
-    artists += playListDictionary["artists"] as! [String]
+    artists += playlistDictionary["artists"] as! [String]
     
-    let colorsDictionary = playListDictionary["backgroundColor"] as! [String: CGFloat]
+    let colorsDictionary = playlistDictionary["backgroundColor"] as! [String:CGFloat]
     backgroundColor = rgbColorFromDictionary(colorsDictionary)
   }
   
-  func rgbColorFromDictionary(colorDictionary: [String: CGFloat]) -> UIColor {
+  func rgbColorFromDictionary(colorDictionary: [String:CGFloat]) -> UIColor {
     let red = colorDictionary["red"]!
     let green = colorDictionary["green"]!
     let blue = colorDictionary["blue"]!
     let alpha = colorDictionary["alpha"]!
-
-    return UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: alpha/255.0)
+    
+    return UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: alpha)
   }
   
 }
